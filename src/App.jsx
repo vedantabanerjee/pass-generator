a1e5fd7a249ad8c15e2da19e9b7bfb9a69455199
vedantabanerjee <vedantabanerjee01@gmail.com>
2024-07-15 02:41:39 +0530

    v1.0.2

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef hook
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+  return (
+    <div className="pt-10 px-8 sm:px-0">
+      <h1 className="text-black text-4xl text-center my-3 ">Password Generator</h1>
+      <h3 className=" text-gray-700 text-center pb-64">Generate password of any length and type! </h3>
+      <div className="w-full max-w-xl mx-auto shadow-md rounded-lg px-5 pt-5 py-3 my-8 bg-gray-800 text-purple-400">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          >
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Include Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Include Special Characters</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-2 fixed bottom-0 w-full">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+        <p className="text-center mb-2 md:mb-0">
+          &copy; {new Date().getFullYear()} vedanta
+        </p>
+        <div className='flex'>
+        <p className='text-gray-300 pr-10'>version 1.0.2</p>
+          <a
+            href="https://github.com/vedantabanerjee/pass-generator"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center text-white hover:text-gray-400 transition duration-300 pr-5"
+          >
+            <FaGithub className="mr-2" />
+            GitHub Repository
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
